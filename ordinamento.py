bf9fd354390631106e3b887090de0f6eee4212ac
manuela <manuela99183@gmail.com>
2025-12-03 19:46:44 +0100

    ordinamento

---
+++ ordinamento.py
@@ -0,0 +1,10 @@
+prezzi_lista = [45.5, 12.0, 78.3, 23.1, 56.7]
+print("Prezzi originali", prezzi_lista)
+numeri_ordinati = sorted(prezzi_lista)
+print("Prezzi ordinati", numeri_ordinati)
+print("Minimo", min(prezzi_lista))
+print("Massimo",max(prezzi_lista))
+
+check_item = 23.1
+controllo: bool = check_item in prezzi_lista
+print(check_item, controllo)
